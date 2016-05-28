77c019557ce23b2537b94b671b14cff0472ee155
Andreas Bader <Development@Geekparadise.de>
2016-05-28 22:54:02 +0200

    Fix setting hostname on none cloud-init based providers

--- vagrant_files/basic/change_hostname.rb
+++ vagrant_files/basic/change_hostname.rb
@@ -1,4 +1,4 @@
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provision "shell", inline: "/root/change_hostname.sh #{HOSTNAME}"
-  config.vm.provision "shell", inline: "sed -i 's/\(manage_etc_hosts:\).*/\1 false/' /etc/cloud/cloud.cfg"
+  config.vm.provision "shell", inline: "if [ -f /etc/cloud/cloud.cfg ]; then sed -i 's/\(manage_etc_hosts:\).*/\1 false/' /etc/cloud/cloud.cfg; fi"
 end
