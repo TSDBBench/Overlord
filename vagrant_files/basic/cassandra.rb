c7db1b8f16c9ad2d5c3759b77094c89c187e9247
Andreas Bader <Development@Geekparadise.de>
2016-05-28 22:29:45 +0200

    Fix Cassandra based databases

--- vagrant_files/basic/cassandra.rb
+++ vagrant_files/basic/cassandra.rb
@@ -7,4 +7,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provision "shell", inline: "systemctl stop cassandra.service"
   config.vm.provision "shell", inline: "if [ $(ps ax | grep \"cassandra\" | grep -v \"grep\" | wc -l) -ne 0 ]; then for pid in $(ps ax | grep \"cassandra\" | grep -v \"grep\" | grep -Eo \"^[[:blank:]]*[0-9]{1,7}\" | sed \"s:[[:blank:]]::g\"); do kill -9 $pid; done; fi"
   config.vm.provision "shell", inline: "rm -rf /var/lib/cassandra/*"
+  # Fix for JAVA Bug when Java is version is over 100
+  # see https://issues.apache.org/jira/browse/CASSANDRA-11661
+  # should be fixed with  2.1.15 
+  config.vm.provision "shell", inline: "sed -i 's|\\[ \"$JVM_PATCH_VERSION\" \\\\< \"25\" \\]|\\[ \"$JVM_PATCH_VERSION\" -lt \"25\" \\]|g' /etc/cassandra/cassandra-env.sh"
 end
