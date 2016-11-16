4b4bf5a070c0cc87613b119592735bb19fcc5c23
Andreas Bader <Development@Geekparadise.de>
2016-11-16 18:27:12 +0100

    Update vagrantconf.rb

--- vagrant_files/vagrantconf.rb
+++ vagrant_files/vagrantconf.rb
@@ -21,6 +21,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     openstack.image              = ''
     openstack.floating_ip_pool   = ''
     openstack.openstack_image_url = ''
+    openstack.security_groups     = ['default','allow_all']
+    # allow_all or default must at least provide SSH (22) ingress access!
   end
   config.vm.provider :virtualbox do |virtualbox, override|
     override.vm.box                = "tsdbbench-debian"
